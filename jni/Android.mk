f276e4245f6ba47ca641b045b2e0ceb27f421356
Cassius de Oliveira Puodzius <puodzius@gmail.com>
2014-11-22 04:22:55 -0200

    Including NTRU, ECDSA and SPONGE to compilation

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,11 +9,42 @@ CRYPTO_PROJ_SRC := 				\
 	$(CRYPTO_PROJ_PATH)/src/util.c
 CRYPTO_PROJ_FLAGS := -DAES_ENC_DEC -DAES_CBC_MODE 
 
+SPONGE_PROJ_PATH := $(CRYPTO_PROJ_PATH)/modules/Hash-Functions-8-16-bits
+SPONGE_PROJ_INCLUDE := $(SPONGE_PROJ_PATH) $(SPONGE_PROJ_PATH)/sponge_functions
+SPONGE_PROJ_SRC :=						\
+	$(SPONGE_PROJ_PATH)/sponge_functions/keccak8bits.c	\
+	$(SPONGE_PROJ_PATH)/sponge8bits.c	
+
+CERTIFICATE_PROJ_PATH := $(LOCAL_PATH)/certificate
+CERTIFICATE_PROJ_SRC :=				\
+	$(CERTIFICATE_PROJ_PATH)/certificate.c	\
+	$(CERTIFICATE_PROJ_PATH)/cert_time.c	\
+	$(CERTIFICATE_PROJ_PATH)/ecdsa.c	\
+	$(CERTIFICATE_PROJ_PATH)/ntru.c
+
+NTRU_PROJ_PATH := $(CERTIFICATE_PROJ_PATH)/libntru
+NTRU_PROJ_SRC :=				\
+	$(NTRU_PROJ_PATH)/src/ntru.c		\
+	$(NTRU_PROJ_PATH)/src/bitstring.c	\
+	$(NTRU_PROJ_PATH)/src/encparams.c	\
+	$(NTRU_PROJ_PATH)/src/hash.c		\
+	$(NTRU_PROJ_PATH)/src/idxgen.c		\
+	$(NTRU_PROJ_PATH)/src/key.c		\
+	$(NTRU_PROJ_PATH)/src/mgf.c		\
+	$(NTRU_PROJ_PATH)/src/poly.c		\
+	$(NTRU_PROJ_PATH)/src/rand.c		\
+	$(NTRU_PROJ_PATH)/src/sha1.c		\
+	$(NTRU_PROJ_PATH)/src/sha2.c
+
+uECC_PROJ_PATH := $(CERTIFICATE_PROJ_PATH)/micro-ecc
+uECC_PROJ_SRC := $(uECC_PROJ_PATH)/uECC.c
+
 #build native lib
 include $(CLEAR_VARS)
 LOCAL_MODULE := crypto-library
-LOCAL_C_INCLUDES += $(CRYPTO_PROJ_PATH)/include
+LOCAL_C_INCLUDES += $(CRYPTO_PROJ_PATH)/include $(SPONGE_PROJ_INCLUDE) $(CERTIFICATE_PROJ_PATH) $(NTRU_PROJ_PATH)/src $(uECC_PROJ_PATH) 
 LOCAL_CFLAGS += $(CRYPTO_PROJ_FLAGS)
-LOCAL_SRC_FILES := br_usp_larc_sembei_capacitysharing_crypto_MSS.c br_usp_larc_sembei_capacitysharing_crypto_MSSCryptoProvider.c $(CRYPTO_PROJ_SRC)
+LOCAL_SRC_FILES :=	br_usp_larc_sembei_capacitysharing_crypto_MSS.c br_usp_larc_sembei_capacitysharing_crypto_MSSCryptoProvider.c	\
+			$(CRYPTO_PROJ_SRC) $(SPONGE_PROJ_SRC) $(CERTIFICATE_PROJ_SRC) $(NTRU_PROJ_SRC) $(uECC_PROJ_SRC)
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,1 +1,8 @@
-APP_ABI := all
+APP_ABI := 		\
+	armeabi-v7a	\
+	x86		\
+	x86_64
+	#arm64-v8a	\
+	#armeabi		\
+	#mips		\
+	#mips64		\
